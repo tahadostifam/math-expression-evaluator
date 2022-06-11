9a4f8e72373fe6baaca8183ef23003130babd14b
Taha Dostifam <mr.tahadostifam@gmail.com>
2022-06-11 13:58:38 +0430

    .

--- src/main.rb
+++ src/main.rb
@@ -1,4 +1,4 @@
-require_relative 'math__solver.rb'
+require_relative "math__solver.rb"
 
 PROMPT = "> "
 RESULT = "# -> "
@@ -6,19 +6,19 @@ RESULT = "# -> "
 include MathSolver
 
 def main
-    while true
-        print PROMPT
-        input = gets
-        case input.strip
-        when 'exit'
-            exit
-        when 'clear', 'cls'
-            Gem.win_platform? ? (system "cls") : (system "clear")
-        else
-            result = MathSolver.solve(input)
-             puts "# -> #{result}"
-        end
+  while true
+    print PROMPT
+    input = gets
+    case input.strip
+    when "exit"
+      exit
+    when "clear", "cls"
+      Gem.win_platform? ? (system "cls") : (system "clear")
+    else
+      result = MathSolver.solve(input)
+      puts "# -> #{result}"
     end
+  end
 end
 
 main
--- src/math__solver.rb
+++ src/math__solver.rb
@@ -1,187 +1,11 @@
 module MathSolver
-    @expr_arr = []
-
-    def self.solve(expr)
-      final_result = nil
-      @expr_arr = expr
-      if valid_expr?
-        @expr_arr = separate_characters!
-        @expr_arr = string_to_int!
-
-        @expr_arr = evalSqrt!
-
-        @expr_arr = evalDevisionExpr!
-        @expr_arr = evalMultipExpr!
-        @expr_arr = evalMinusExpr!
-        @expr_arr = evalPlusExpr!
-
-        final_result = @expr_arr.join("")
-      else
-        puts "Invalid Syntax!"
-        exit
-      end
-      final_result
-    end
-    
-    private
-    
-    def evalSqrt!
-      i = 0
-      coo = count_of_ops!("sqrt")
-
-      puts "coo of sqrt: #{coo}" # NOTE
-
-      if coo > 0
-        while coo > 0
-          current_item = @expr_arr[i]
-          
-          if current_item.include? "sqrt"
-            sqrt_val = nil
-            open_bracket_i = i + 1
-
-            while true
-              if @expr_arr[open_bracket_i] == ")"
-                sqrt_val = @expr_arr[(i + 1)..open_bracket_i]
-                break
-              end
-              open_bracket_i += 1
-            end
-
-            puts "sqrt_val: #{sqrt_val.to_s}"
-
-            @expr_arr[(i - 1)..(i + 1)] = 100
-            i = i - 1
-  
-            coo -= 1
-          end
-  
-          i += 1
-        end
-      end
-
-      @expr_arr
-    end
-
-    def evalDevisionExpr!
-      i = 0
-      coo = count_of_ops!("/")
-
-      if coo > 0
-        while coo > 0
-          current_item = @expr_arr[i]
-          
-          if current_item == "/"
-            result = nil
-            prev_word = @expr_arr[i - 1].to_i
-            next_word = @expr_arr[i + 1].to_i
-            
-            result = nil
-            begin
-              result = prev_word / next_word
-            rescue ZeroDivisionError => e
-              puts "ZeroDivisionError"
-              exit
-            end
-            @expr_arr[(i - 1)..(i + 1)] = result
-            i = i - 1
-  
-            coo -= 1
-          end
-  
-          i += 1
-        end
-      end
-
-      @expr_arr
-    end
-
-    def evalMultipExpr!
-      i = 0
-      coo = count_of_ops!("*")
-
-      if coo > 0
-        while coo > 0
-          current_item = @expr_arr[i]
-          
-          if current_item == "*"
-            result = nil
-            prev_word = @expr_arr[i - 1].to_i
-            next_word = @expr_arr[i + 1].to_i
-            
-            result = prev_word * next_word
-            @expr_arr[(i - 1)..(i + 1)] = result
-            i = i - 1
-  
-            coo -= 1
-          end
-  
-          i += 1
-        end
-      end
-
-      @expr_arr
-    end
-
-    def evalMinusExpr!
-      i = 0
-      coo = count_of_ops!("-")
-
-      if coo > 0
-        while coo > 0
-          current_item = @expr_arr[i]
-          
-          if current_item == "-"
-            result = nil
-            prev_word = @expr_arr[i - 1].to_i
-            next_word = @expr_arr[i + 1].to_i
-            
-            result = prev_word - next_word
-            @expr_arr[(i - 1)..(i + 1)] = result
-            i = i - 1
-  
-            coo -= 1
-          end
-  
-          i += 1
-        end
-      end
-
-      @expr_arr
-    end
-
-    def evalPlusExpr!
-      i = 0
-      coo = count_of_ops!("+")
-
-      if coo > 0
-        while coo > 0
-          current_item = @expr_arr[i]
-          
-          if current_item == "+"
-            result = nil
-            prev_word = @expr_arr[i - 1].to_i
-            next_word = @expr_arr[i + 1].to_i
-            
-            result = prev_word + next_word
-            @expr_arr[(i - 1)..(i + 1)] = result
-            i = i - 1
-  
-            coo -= 1
-          end
-  
-          i += 1
-        end
-      end
-
-      @expr_arr
-    end
-
-    def count_of_ops!(op)
+  class Lib
+    def self.count_of_operators!(expr_arr, op)
       i = 0
       c = 0
 
-      while i < @expr_arr.length
-        if @expr_arr[i].to_s.include? op || is_valid_keyword?(op)
+      while i < expr_arr.length
+        if expr_arr[i].to_s.include? op || is_valid_keyword?(expr_arr, op)
           c += 1
         end
         i += 1
@@ -189,63 +13,63 @@ module MathSolver
       c
     end
 
-    def string_to_int!
+    def self.string_to_int!(expr_arr)
       i = 0
-      while i < @expr_arr.length
-        current_item = @expr_arr[i]
+      while i < expr_arr.length
+        current_item = expr_arr[i]
 
-        if !MathSolver.operators.include?(current_item)
-          @expr_arr[i] = current_item.to_i
+        if !MathSolver::Lib.include_ops? current_item
+          expr_arr[i] = current_item.to_i
         end
 
         i += 1
       end
-      @expr_arr
+      expr_arr
     end
 
-    def separate_characters!
-      wws = [] # without_white_space
-      wws_i = 0
-      while wws_i < @expr_arr.length
-      current_char = @expr_arr[wws_i].to_s.strip
-      if current_char.length > 0
-        wws << current_char
-      end
-      wws_i += 1
-      end
-      sc = [] # separated_chars
-      sc_i = 0
-      lo_i = 0 # last operator's index
-      while sc_i < wws.length + 1
-      current_char = wws[sc_i]
-      if include_ops?(current_char)
-        fn_i = sc_i - 1 # finish of word's index
-        if include_ops?(wws[sc_i + 1])
-          fn_i = sc_i
+    def self.separate_characters!(expr_arr)
+      without_white_space_arr = []
+      without_white_space_index = 0
+      while without_white_space_index < expr_arr.length
+        current_char = expr_arr[without_white_space_index].to_s.strip
+        if current_char.length > 0
+          without_white_space_arr << current_char
         end
-        sc << wws[lo_i..fn_i].join("")
-        sc << current_char
-        lo_i = sc_i + 1
+        without_white_space_index += 1
       end
+      separated_chars = []
+      separated_chars_index = 0
+      last_operator_index = 0
+      while separated_chars_index < without_white_space_arr.length + 1
+        current_char = without_white_space_arr[separated_chars_index]
+        if include_ops?(current_char)
+          fn_i = separated_chars_index - 1 # finish of word's index
+          if include_ops?(without_white_space_arr[separated_chars_index + 1])
+            fn_i = separated_chars_index
+          end
+          separated_chars << without_white_space_arr[last_operator_index..fn_i].join("")
+          separated_chars << current_char
+          last_operator_index = separated_chars_index + 1
+        end
 
-      if sc_i + 1 >= wws.length + 1
-        # finish of chars
-        sc << wws[lo_i..sc_i].join("")
+        if separated_chars_index + 1 >= without_white_space_arr.length + 1
+          # finish of chars
+          separated_chars << without_white_space_arr[last_operator_index..separated_chars_index].join("")
+        end
+
+        separated_chars_index += 1
       end
-      
-      sc_i += 1
-      end
-      sc
+      separated_chars
     end
 
-    def is_valid_keyword?(inp)
-      separated_chars_arr = separate_characters!
-      ret = true
+    def self.is_valid_keyword?(expr_arr, inp)
+      separated_chars_arr = separate_characters!(expr_arr)
+      return_value = true
       i = 0
       while i < separated_chars_arr.length
         item = separated_chars_arr[i]
         if !item.to_s.include?(inp.to_s)
-          ret = false
+          return_value = false
         end
         for valid_char in valid_chars
           if valid_char.include?(item)
@@ -254,17 +78,16 @@ module MathSolver
         end
         i += 1
       end
-      puts "ret: #{ret}"
-      ret
+      return_value
     end
 
-    def valid_expr?
+    def self.valid_expr?(expr_arr)
       result = true
       i = 0
-      while i < @expr_arr.length
-        item = @expr_arr[i].to_s
-        
-        if item.strip.length > 0 && !valid_chars.include?(item) && !is_valid_keyword?(item)
+      while i < expr_arr.length
+        item = expr_arr[i].to_s
+
+        if item.strip.length > 0 && !valid_chars.include?(item) && !is_valid_keyword?(expr_arr, item)
           result = false
         end
         i += 1
@@ -273,15 +96,78 @@ module MathSolver
       result
     end
 
-    def valid_chars
-      ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "(", ")"] + MathSolver.operators
+    def self.valid_chars
+      ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "(", ")"] + MathSolver::Lib.operators
     end
 
-    def include_ops?(inp)
-      MathSolver.operators.include?(inp)
+    def self.include_ops?(inp)
+      MathSolver::Lib.operators.include?(inp)
     end
 
     def self.operators
       return ["+", "-", "*", "/", "sqrt"]
     end
+  end
+
+  class OperatorEvalulator
+    def self.start_solve!(expr_arr, op)
+      i = 0
+      coo = MathSolver::Lib.count_of_operators!(expr_arr, op)
+
+      if coo > 0
+        while coo > 0
+          current_item = expr_arr[i]
+
+          if current_item == op
+            result = nil
+            prev_word = expr_arr[i - 1].to_i
+            next_word = expr_arr[i + 1].to_i
+
+            result = 0
+            case op
+            when "+"
+              result = prev_word + next_word
+            when "-"
+              result = prev_word - next_word
+            when "*"
+              result = prev_word * next_word
+            when "/"
+              result = prev_word / next_word
+            else
+              raise "bad operator"
+            end
+            expr_arr[(i - 1)..(i + 1)] = result
+            i = i - 1
+
+            coo -= 1
+          end
+
+          i += 1
+        end
+      end
+
+      expr_arr
+    end
+  end
+
+  def self.solve(expr)
+    final_result = nil
+
+    if MathSolver::Lib.valid_expr? expr
+      expr = MathSolver::Lib.separate_characters!(expr)
+      expr = MathSolver::Lib.string_to_int!(expr)
+
+      MathSolver::OperatorEvalulator.start_solve!(expr, "/")
+      MathSolver::OperatorEvalulator.start_solve!(expr, "*")
+      MathSolver::OperatorEvalulator.start_solve!(expr, "-")
+      MathSolver::OperatorEvalulator.start_solve!(expr, "+")
+
+      final_result = expr.join("")
+    else
+      puts "Invalid Syntax!"
+      exit
+    end
+
+    final_result
+  end
 end
